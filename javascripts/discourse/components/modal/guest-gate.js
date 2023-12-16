7ce7fa5448e4176e834c812def345d2dd492cbaa
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-12-16 12:46:42 +0100

    DEV: Remove unused actions

--- javascripts/discourse/components/modal/guest-gate.js
+++ javascripts/discourse/components/modal/guest-gate.js
@@ -1,41 +1,11 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import getURL from "discourse-common/lib/get-url";
-import LoginModal from "discourse/components/modal/login";
-import showModal from "discourse/lib/show-modal";
 import { getOwner } from "@ember/application";
 import { inject as service } from "@ember/service";
 
 export default class extends Component {
   @service modal;
-  
-  @action
-  showLoginGate(event) {
-    event?.preventDefault();
-    this.modal.show(LoginModal, {
-      model: {
-        showNotActivated: (props) => getOwner(this).lookup("controller:application").send("showNotActivated", props),
-        showCreateAccount: (props) => getOwner(this).lookup("controller:application").send("showCreateAccount", props),
-        canSignUp: getOwner(this).lookup("controller:application").canSignUp,
-      },
-    });
-  }
-    
-  @action
-  ssoLoginGate(event) {
-    event?.preventDefault();
-    const returnPath = encodeURIComponent(window.location.pathname);
-    window.location = getURL("/session/sso?return_path=" + returnPath);
-  }
-    
-  @action
-  showCreateAccountGate(event) {
-    event?.preventDefault();
-    showModal("createAccount", {
-      modalClass: "create-account",
-      titleAriaElementId: "create-account-title",
-    });
-  }
   
   @action
   externalLogin(provider) {
